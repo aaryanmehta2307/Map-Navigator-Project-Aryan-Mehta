23e88510f9bcbea04ce6f71a2dcfc6ed874724df
aaryanmehta2307 <118744251+aaryanmehta2307@users.noreply.github.com>
2023-10-30 01:13:16 +0530

    blocks setup

--- map-navigator-app/src/pathfindingvisualizer/PathfindingVisualizer.jsx
+++ map-navigator-app/src/pathfindingvisualizer/PathfindingVisualizer.jsx
@@ -1,20 +1,56 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Node from './Node/Node';
 
 import './PathfindingVisualizer.css';
 
-export default class PathfindingVisualizer extends Component{
-    constructor(props){
+export default class PathfindingVisualizer extends Component {
+    constructor(props) {
         super(props);
-        this.state={};
+        this.state = {
+            nodes: [],
+        };
+    }
+    componentDidMount() {
+        const nodes = [];
+        for (let row = 0; row < 15; row++) {
+            const currentRow = [];
+            for (let col = 0; col < 50; col++) {
+                const currentNode={
+                    col,
+                    row,
+                    isStart:  row===10 && col===5,
+                    isFinish:row===10 && col===45,
+                };
+                currentRow.push(currentNode);
+            }
+            nodes.push(currentRow);
+        }
+        this.setState({ nodes })
     }
 
-    render(){
+    render() {
+        const { nodes } = this.state;
+        console.log(nodes)
         return (
-           <div>
-           Foo
-           <Node></Node>
-           </div>
+            <div className="grid">
+                {nodes.map((row, rowIdx) => {
+                    return(
+                     <div key={rowIdx}>
+                           {row.map((node, nodeIdx) => {
+                            const {isStart,isFinish}=node;
+                            return(
+                             <Node
+                                key={nodeIdx}
+                                isStart={isStart}
+                                isFinish={isFinish}
+                                test={'foo'}>                            </Node>
+                           );
+                        })}
+                    </div>
+                    );
+                })}
+
+            </div>
         );
     }
 }
