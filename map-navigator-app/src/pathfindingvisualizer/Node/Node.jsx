e81053bf8fabbcb514683b6d2bf38364554793be
aaryanmehta2307 <118744251+aaryanmehta2307@users.noreply.github.com>
2023-11-07 09:48:08 +0530

    remove extra content

--- map-navigator-app/src/pathfindingvisualizer/Node/Node.jsx
+++ map-navigator-app/src/pathfindingvisualizer/Node/Node.jsx
@@ -27,7 +27,7 @@ export default class Node extends Component {
                 className={`node ${extraClassName}`}
                 onMouseDown={() => onMouseDown(row, col)}
                 onMouseEnter={() => onMouseEnter(row, col)}
-                onMouseUp={() => onMouseUp()} // Fix the typo here
+                onMouseUp={() => onMouseUp()} 
             ></div>
         );
     }
